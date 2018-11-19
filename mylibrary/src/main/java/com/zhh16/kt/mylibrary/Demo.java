3e59dc982ab7e6da919febf231475250336247dc
zhh16 <zhh163cool@163.com>
2018-11-19 23:27:38 +0800

    test demo.java file.

---
+++ mylibrary/src/main/java/com/zhh16/kt/mylibrary/Demo.java
@@ -0,0 +1,14 @@
+package com.zhh16.kt.mylibrary;
+
+import android.util.Log;
+
+public class Demo {
+
+    static final String TAG = Demo.class.getSimpleName();
+
+    public void printTest(){
+            Log.d(TAG, "1   1qwa2qwdvgcfv765: ");
+    }
+
+
+}
